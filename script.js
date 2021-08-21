0968aee20fa347343e354905b3553d02afebe674
Mohit khadayat <mohitkhadayat06@gmail.com>
2021-08-21 16:19:47 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const months = ['January', 'February','March','April','May', 'June', 'July','August','September', 'October','November','December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation){
+ navigator.geolocation.getCurrentPosition(function(position){
+   
+     const {latitude, longtitude} = position.coords;
+     const coords = [latitude, longtitude]
+    
+ //  console.log('https://www.google.com/maps/@{latitude},{longitude},13z')
+   const map = L.map('map').setView(coords, 13);
+
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+ L.marker(coords).addTo(map)
+    .bindPopup('This is a marker')
+    .openPopup();
+   }, function(){
+    console.log("cannot access your coordinates")
+ })
+}
